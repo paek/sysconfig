8f9062a92fcb3e601243170e6fffb166acae3477
Hung-Ju, Baek <acegame@gmail.com>
2018-08-08 11:13:51 +0900

    Create alive-check.sh
    running : 1, not running : 0 return.
    service 명으로 daemon 프로세서인지 체크하고 없으면 구동중인 프로세서 리스트에서 없는지 한번더 체크 함.
    
    service daemon process check. not process running list retry again check.

---
+++ alive-check.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# ./alive-check.sh $DAEMON_NAME
+# running : 1, not running : 0 return.
+
+
+if [ -z $1 ]; then
+    echo "proceess not found."
+    exit 1
+fi
+
+PROCESS="$1"
+PID="/var/run/$1"
+
+if [ -f "$PID" ]
+then
+        PROCPID=`service $PROCESS status | awk '{print $3}' | sed 's/)//g'`
+        ALIVECHK=`service $PROCESS status | grep -c $PROCPID`
+        echo "$ALIVECHK";
+        exit
+        #echo "PROCPID - $PROCPID"
+else
+        PROC2PID=`ps -ef | pgrep ${PROCESS:0:14}`
+        if [ -z "$PROC2PID" ]; then
+                echo "0 ($PROCESS) Process daemon not found."
+            exit 1;
+        fi
+        ALIVECHK=`cat /proc/$PROC2PID/status | grep Name | sed 's/Name://g' | grep -c "${PROCESS:0:14}"`
+fi
+
+        echo "$ALIVECHK"
+exit
